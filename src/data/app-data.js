e8ce9b4e208aa417714f52257fa08da470397136
Shahidul <ishahin56@gmail.com>
2022-08-17 13:16:40 +0600

    Added delete feature

---
+++ src/data/app-data.js
@@ -0,0 +1,44 @@
+export const contactsData = [
+   {
+      id: 1,
+      name: "Shahidul Islam",
+      email: "ishahin56@gmail.com",
+      phone: "+8801620290055"
+   },
+   {
+      id: 2,
+      name: "Shahidul Islam",
+      email: "ishahin56@gmail.com",
+      phone: "+8801620290055"
+   },
+   {
+      id: 3,
+      name: "Shahidul Islam",
+      email: "ishahin56@gmail.com",
+      phone: "+8801620290055"
+   },
+   {
+      id: 4,
+      name: "Shahidul Islam",
+      email: "ishahin56@gmail.com",
+      phone: "+8801620290055"
+   },
+   {
+      id: 5,
+      name: "Shahidul Islam",
+      email: "ishahin56@gmail.com",
+      phone: "+8801620290055"
+   },
+   {
+      id: 6,
+      name: "Shahidul Islam",
+      email: "ishahin56@gmail.com",
+      phone: "+8801620290055"
+   },
+   {
+      id: 7,
+      name: "Shahidul Islam",
+      email: "ishahin56@gmail.com",
+      phone: "+8801620290055"
+   }
+]
